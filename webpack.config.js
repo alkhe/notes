5914ebe8130b349cd31e268f90213684dc54b27b
edge <functaire@gmail.com>
2020-06-21 15:08:09 +1000

    update webpack config: extract css in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 const pages = ['micro', 'macro']
 
@@ -11,35 +13,42 @@ function make_page(name) {
   })
 }
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve('./dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.md$/,
-        use: [
-          'html-loader',
-          {
-            loader: 'techdown-loader',
-            options: {
-              nomnoml_prelude: '#font: Source Code Variable'
+module.exports = (env, argv) => {
+  const page_plugins = pages.map(make_page)
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve('./dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.md$/,
+          use: [
+            'html-loader',
+            {
+              loader: 'techdown-loader',
+              options: {
+                nomnoml_prelude: '#font: Source Code Variable'
+              }
             }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.woff2$/,
-        use: 'file-loader'
-      }
-    ]
-  },
-  plugins: pages.map(make_page)
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            argv.mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.woff2$/,
+          use: 'file-loader'
+        }
+      ]
+    },
+    plugins: argv.mode === 'production' ? [new MiniCssExtractPlugin, new OptimizeCSSAssetsPlugin].concat(page_plugins) : page_plugins
+  }
 }
 
